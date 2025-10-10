aad071314765dee15a2136c1e67501f95eff9b3f
Rozycki, Konrad (contracted) <Konrad.Rozycki@contracted.pmi.com>
2025-10-10 12:30:06 +0200

    Configure IRSA

--- sre-challenge/Iac/eks.tf
+++ sre-challenge/Iac/eks.tf
@@ -16,13 +16,13 @@ resource "aws_eks_cluster" "this" {
     }
   }
 
-  depends_on = [module.vpc, module.vpc.subnet_ids]
+  depends_on = [module.vpc]
 }
 
 resource "aws_eks_node_group" "node_group" {
   cluster_name    = aws_eks_cluster.this.name
   node_group_name = "sre-challenge-eks-node-group"
-  node_role_arn   = "arn:aws:iam::${var.aws_account_id}:role/eks-node-group-role"
+  node_role_arn   = aws_iam_role.eks_node_group_role.arn
   subnet_ids      = [local.subnets.eks]
 
   scaling_config {
@@ -34,4 +34,21 @@ resource "aws_eks_node_group" "node_group" {
   instance_types = ["t3.medium"]
 
   depends_on = [aws_eks_cluster.this]
+}
+
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "sre-challenge-eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
+    ]
+  })
 }
